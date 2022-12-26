f6deb14cc744cd07b0f46d7b8c866cc04225e5ae
rubansundararaj <rubansundararaj@gmail.com>
2022-12-26 17:23:58 +0800

    add lnd combined router

--- birkeland_router/lnd_operation_router.js
+++ birkeland_router/lnd_operation_router.js
@@ -5,9 +5,15 @@ var authenticated_lnd_operation = require("./../birkeland_controller/lnd_grpc_op
 
 var unauthenticated_lnd_operation = require("./../birkeland_controller/lnd_grpc_operation/unauthenticated_operations");
 
+var combined_lnd_grpc_operation = require("./../birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation");
+
 
 apirouter.post("/auth_lnd_ops", authenticated_lnd_operation.PerformAuthenticatedOperation);
 
 apirouter.post("/unauth_lnd_ops", unauthenticated_lnd_operation.PerformUnAuthenticatedOperation);
+
+apirouter.get("/get_lnd_dashboard_info", combined_lnd_grpc_operation.get_lnd_dashboard_info);
+
+
 
 module.exports = apirouter;
