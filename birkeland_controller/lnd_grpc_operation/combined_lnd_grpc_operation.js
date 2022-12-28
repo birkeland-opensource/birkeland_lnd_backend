9ba4170cf8dbc76fe0b6c5c254f26762e52c0205
rubansundararaj <rubansundararaj@gmail.com>
2022-12-28 09:49:28 +0800

    updates

--- birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
+++ birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
@@ -2,32 +2,17 @@ const {
   getWalletInfo,
   authenticatedLndGrpc,
   getChainBalance,
-  getChannel,
   getChannelBalance,
-  getChannels,
-  getMethods,
-  getNode,
-  getNetworkInfo,
-  getPeers,
-  getWalletStatus,
   getWalletVersion,
-  getPublicKey,
-  openChannel,
-  createChainAddress,
-  getUtxos,
-  addPeer,
-  pay,
-  getBackup,
-  getBackups,
 } = require("lightning");
 
 const fs = require("fs");
-const tls_cert = fs.readFileSync("/home/user/temp", {
+const tls_cert = fs.readFileSync("/home/birkeland/temp", {
     encoding: "utf8",
     flag: "r",
   });
 
-const base_sixtyfoir_macroon = fs.readFileSync("/home/user/macaroon", {
+const base_sixtyfoir_macroon = fs.readFileSync("/home/birkeland/macaroon", {
     encoding: "utf8",
     flag: "r",
   });
