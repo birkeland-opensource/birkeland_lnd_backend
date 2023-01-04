8884d73e36d37ae73b7aeb79f61276110cacb3ac
rubansundararaj <rubansundararaj@gmail.com>
2023-01-04 17:43:24 +0800

    update

--- birkeland_router/lnd_operation_router.js
+++ birkeland_router/lnd_operation_router.js
@@ -12,5 +12,6 @@ apirouter.post("/auth_lnd_ops", authenticated_lnd_operation.PerformAuthenticated
 
 apirouter.post("/unauth_lnd_ops", unauthenticated_lnd_operation.PerformUnAuthenticatedOperation);
 
+//http://localhost:9990/terminal/setup_encrypted_config_for_grpc
 apirouter.get("/get_lnd_dashboard_info", combined_lnd_grpc_operation.get_lnd_dashboard_info);
 module.exports = apirouter;
