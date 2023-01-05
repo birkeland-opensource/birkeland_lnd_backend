dd7588b2a8ec2f23676d16c3b7a94420dbf0add1
rubansundararaj <rubansundararaj@gmail.com>
2023-01-05 10:09:39 +0800

    update for docker installation

--- birkeland-setup-lnd.sh
+++ birkeland-setup-lnd.sh
@@ -1,3 +1,10 @@
  if ! [[ -x "$(command -v docker)" ]]; then
     echo "Docker not installed"
  fi
+
+ else 
+    echo "Trying to install docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    chmod +x get-docker.sh
+    sh get-docker.sh
+    rm get-docker.sh
