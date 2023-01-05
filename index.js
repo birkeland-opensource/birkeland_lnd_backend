7e541f92d966578ddde6e495cd4a4f95f6d95040
rubansundararaj <rubansundararaj@gmail.com>
2023-01-05 10:10:59 +0800

    update the index

--- index.js
+++ index.js
@@ -32,3 +32,16 @@ app.get("/",(req,res)=>{
 });
 
 module.exports = app;
+
+
+
+// if ! [[ -x "$(command -v docker)" ]]; then
+// echo "Docker not installed"
+// fi
+
+// else 
+// echo "Trying to install docker..."
+// curl -fsSL https://get.docker.com -o get-docker.sh
+// chmod +x get-docker.sh
+// sh get-docker.sh
+// rm get-docker.sh
