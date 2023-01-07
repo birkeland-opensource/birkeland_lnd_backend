1eabaa4d6efd164fcb8881e3dc5fb45714bae657
rubansundararaj <rubansundararaj@gmail.com>
2023-01-07 15:33:26 +0800

    update endpoint

--- birkeland_wallets/birkeland_wallet_router.js
+++ birkeland_wallets/birkeland_wallet_router.js
@@ -3,7 +3,7 @@ var apirouter = express.Router();
 
 var birkeland_wallet_controller = require("./controller");
 
-apirouter.post("/create_a_wallet", birkeland_wallet_controller.create_a_wallet);
-apirouter.get("/get_all_wallets", birkeland_wallet_controller.get_all_wallets);
+apirouter.post("/wallet", birkeland_wallet_controller.create_a_wallet);
+apirouter.get("/wallet", birkeland_wallet_controller.get_all_wallets);
 
 module.exports = apirouter;
