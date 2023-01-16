1f666ee5f34a37494e00a248bdb8b4b18bc22f98
rubansundararaj <rubansundararaj@gmail.com>
2023-01-16 09:53:43 +0800

    update and clean up

--- birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
+++ birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
@@ -6,6 +6,8 @@ const {
   getWalletVersion,
 } = require("lightning");
 
+const test_birkeland_lnd = require('test_birkeland_lnd')
+
 const fs = require("fs");
 const tls_cert = fs.readFileSync("/etc/birkeland/tlscert", {
     encoding: "utf8",
@@ -40,3 +42,26 @@ exports.get_lnd_dashboard_info = async (req, res) => {
     return res.status(500).send({ success: false, message: err });
   }
 };
+
+exports.get_dashboard_info_from_birkeland_lnd = async(req,res) =>{
+
+  try{
+
+    let chain_balance = await test_birkeland_lnd.PerformAuthenticatedOperation({operation : "get_chain_balance"});
+      let channel_balance = await test_birkeland_lnd.PerformAuthenticatedOperation({operation : "get_channel_balance"});
+      let wallet_info = await test_birkeland_lnd.PerformAuthenticatedOperation({operation : "get_wallet_info"});
+      let wallet_version = await test_birkeland_lnd.PerformAuthenticatedOperation({operation : "get_wallet_version"});
+      let dash_board_info_object = {
+        "chain_balance" :chain_balance,
+        "channel_balance" :channel_balance,
+        "wallet_info" :wallet_info,
+        "wallet_version" :wallet_version
+      }
+
+      return res.status(200).send({ success: true, message: dash_board_info_object });
+
+  }catch(err){
+    return res.status(500).send({ success: false, message: err });
+  }
+
+}
--- birkeland_router/lnd_operation_router.js
+++ birkeland_router/lnd_operation_router.js
@@ -1,23 +1,15 @@
 var express = require("express");
 var apirouter = express.Router();
 
-var authenticated_lnd_operation = require("./../birkeland_controller/lnd_grpc_operation/authenticated_operations");
-
 var unauthenticated_lnd_operation = require("./../birkeland_controller/lnd_grpc_operation/unauthenticated_operations");
 
 var combined_lnd_grpc_operation = require("./../birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation");
-const { auth_lnd_ops } = require("../birkeland_controller/test_birkeland_lnd_controller");
-
-
-//apirouter.post("/auth_lnd_ops", authenticated_lnd_operation.perform_authenticated_operation_api);
+const { auth_lnd_ops } = require("../birkeland_controller/birkeland_lnd_controller");
 
 apirouter.post("/auth_lnd_ops", auth_lnd_ops);
 
-
-apirouter.post("/test_birkeland_auth_lnd_ops", auth_lnd_ops);
-
 apirouter.post("/unauth_lnd_ops", unauthenticated_lnd_operation.PerformUnAuthenticatedOperation);
 
-//http://localhost:9990/terminal/setup_encrypted_config_for_grpc
-apirouter.get("/get_lnd_dashboard_info", combined_lnd_grpc_operation.get_lnd_dashboard_info);
+apirouter.get("/get_lnd_dashboard_info", combined_lnd_grpc_operation.get_dashboard_info_from_birkeland_lnd);
+
 module.exports = apirouter;
