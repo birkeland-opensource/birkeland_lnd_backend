45731e80426fa5af715b5ecee5db88216258c1cf
rubansundararaj <rubansundararaj@gmail.com>
2023-01-17 16:03:08 +0800

    iterate through list

--- support_functions/polling_service.js
+++ support_functions/polling_service.js
@@ -5,7 +5,17 @@ const poll_and_update_on_chain_transaction = async () => {
     console.log("poll_and_update_on_chain_transaction")
     let filter = { transaction_confirmed: false };
     let resp = await topup_birkeland_wallet_item.find(filter);
-    console.log(resp);
+    for (let count =0;count <resp.length; i++) {
+        let filter_object = {
+            chain_address : resp["chain_address"],
+            public_key : resp["public_key"],
+            wallet_id : resp["wallet_id"],
+            user_id : resp["user_id"]
+        }
+        console.log(filter_object)
+
+    }
+    console.log("---------------------------------")
   } catch (err) {
     console.log(err);
   }
