e4f75583b627ce08a89e6be13f3fbfc2aa493628
rubansundararaj <rubansundararaj@gmail.com>
2023-01-17 16:20:06 +0800

    add get utxos

--- support_functions/polling_service.js
+++ support_functions/polling_service.js
@@ -1,16 +1,20 @@
+const { LND_GRPC_OPERATION } = require("test_birkeland_lnd/operations");
 const topup_birkeland_wallet_item = require("./../birkeland_controller/../birkeland_wallets/topup_birkeland_wallet_item");
 
 const poll_and_update_on_chain_transaction = async () => {
   try {
     console.log("poll_and_update_on_chain_transaction")
     let filter = { transaction_confirmed: false };
+    
     let resp = await topup_birkeland_wallet_item.find(filter);
-    for (let count =0;count <resp.length; i++) {
+    let chain_balance = await test_birkeland_lnd.PerformAuthenticatedOperation({operation : LND_GRPC_OPERATION.GET_U_TXOS});
+    console.log(chain_balance)
+    for (let count =0;count <resp.length; count++) {
         let filter_object = {
-            chain_address : resp["chain_address"],
-            public_key : resp["public_key"],
-            wallet_id : resp["wallet_id"],
-            user_id : resp["user_id"]
+            chain_address : resp[count]["chain_address"],
+            public_key : resp[count]["public_key"],
+            wallet_id : resp[count]["wallet_id"],
+            user_id : resp[count]["user_id"]
         }
         console.log(filter_object)
 
