130f45994202086138a825519a957774b8c9d7aa
rubansundararaj <rubansundararaj@gmail.com>
2023-01-17 18:35:36 +0800

    test

--- support_functions/polling_service.js
+++ support_functions/polling_service.js
@@ -7,20 +7,22 @@ const poll_and_update_on_chain_transaction = async () => {
     console.log("poll_and_update_on_chain_transaction")
     let filter = { transaction_confirmed: false };
     
-    let resp = await topup_birkeland_wallet_item.find(filter);
-    let chain_balance = await PerformAuthenticatedOperation({operation : LND_GRPC_OPERATION.GET_U_TXOS});
-    console.log(chain_balance)
-    for (let count =0;count <resp.length; count++) {
-        let filter_object = {
-            chain_address : resp[count]["chain_address"],
-            public_key : resp[count]["public_key"],
-            wallet_id : resp[count]["wallet_id"],
-            user_id : resp[count]["user_id"]
-        }
-        console.log(filter_object)
-
-    }
+    let birkeland_wallet_topup_tx = await topup_birkeland_wallet_item.find(filter);
+    let utxos = await PerformAuthenticatedOperation({operation : LND_GRPC_OPERATION.GET_U_TXOS});
+    console.log(birkeland_wallet_topup_tx)
     console.log("---------------------------------")
+    console.log(utxos)
+    // for (let count =0;count <resp.length; count++) {
+    //     let filter_object = { 
+    //         chain_address : resp[count]["chain_address"],
+    //         public_key : resp[count]["public_key"],
+    //         wallet_id : resp[count]["wallet_id"],
+    //         user_id : resp[count]["user_id"]
+    //     }
+    //     console.log(filter_object)
+
+    // }
+   
   } catch (err) {
     console.log(err);
   }
