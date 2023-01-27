e3c8eb463c988e3f50e13027c5aa87257eb3c0db
rubansundararaj <rubansundararaj@gmail.com>
2023-01-27 18:48:53 +0700

    change from post to get

--- birkeland_wallets/birkeland_wallet_router.js
+++ birkeland_wallets/birkeland_wallet_router.js
@@ -9,7 +9,7 @@ apirouter.post("/wallet", decode_node_auth_jwt_token,birkeland_wallet_controller
 apirouter.get("/wallets",decode_node_auth_jwt_token, birkeland_wallet_controller.get_all_wallets);
 apirouter.get("/wallet",decode_node_auth_jwt_token, birkeland_wallet_controller.get_one_wallet);
 
-apirouter.post("/get_onchain_address", controller_birkeland_payments.topup_wallet);
+apirouter.get("/get_onchain_address", controller_birkeland_payments.topup_wallet);
 apirouter.get("/get_wallet_topup_tx",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx);
 apirouter.get("/get_wallet_topup_tx_status",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx_status);
 
