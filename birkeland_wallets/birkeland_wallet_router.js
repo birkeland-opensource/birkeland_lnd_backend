fbe7ee73a3f6b9aa4b1bb483f224f5582fbe0891
rubansundararaj <rubansundararaj@gmail.com>
2023-01-27 14:45:13 +0700

    secure the endpoints

--- birkeland_wallets/birkeland_wallet_router.js
+++ birkeland_wallets/birkeland_wallet_router.js
@@ -10,12 +10,12 @@ apirouter.get("/wallets", birkeland_wallet_controller.get_all_wallets);
 apirouter.get("/wallet", birkeland_wallet_controller.get_one_wallet);
 
 apirouter.post("/get_onchain_address", controller_birkeland_payments.topup_wallet);
-apirouter.get("/get_wallet_topup_tx", controller_birkeland_payments.get_wallet_topup_tx);
-apirouter.get("/get_wallet_topup_tx_status", controller_birkeland_payments.get_wallet_topup_tx_status);
+apirouter.get("/get_wallet_topup_tx",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx);
+apirouter.get("/get_wallet_topup_tx_status",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx_status);
 
 
 
-apirouter.get("/transactions", controller_birkeland_payments.transactions);
+apirouter.get("/transactions",auth_birkeland_wallet_access, controller_birkeland_payments.transactions);
 apirouter.post("/create_invoice",auth_birkeland_wallet_access, controller_birkeland_payments.create_invoice);
 apirouter.post("/make_a_payment", auth_birkeland_wallet_access,controller_birkeland_payments.make_a_payment);
 apirouter.get("/decode_lightning_invoice", controller_birkeland_payments.decode_lightning_invoice);
