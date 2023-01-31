d1fcc5d6809cf15e8d165240b3ffa0b68843fec0
rubansundararaj <rubansundararaj@gmail.com>
2023-01-31 12:32:05 +0800

    add get count

--- birkeland_wallets/birkeland_wallet_router.js
+++ birkeland_wallets/birkeland_wallet_router.js
@@ -8,11 +8,16 @@ var controller_birkeland_payments = require("./controller_birkeland_payments");
 apirouter.post("/wallet", decode_node_auth_jwt_token,birkeland_wallet_controller.create_a_wallet);
 apirouter.get("/wallets",decode_node_auth_jwt_token, birkeland_wallet_controller.get_all_wallets);
 apirouter.get("/wallet",decode_node_auth_jwt_token, birkeland_wallet_controller.get_one_wallet);
+apirouter.get("/all_transactions",decode_node_auth_jwt_token, controller_birkeland_payments.all_transactions);
+
 
 apirouter.get("/get_onchain_address", controller_birkeland_payments.topup_wallet);
 apirouter.get("/get_wallet_topup_tx",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx);
 apirouter.get("/get_wallet_topup_tx_status",auth_birkeland_wallet_access, controller_birkeland_payments.get_wallet_topup_tx_status);
 apirouter.post("/withdraw_to_onchain_address",auth_birkeland_wallet_access, controller_birkeland_payments.withdraw_to_onchain_address);
+
+
+apirouter.post("/all_transactions",decode_node_auth_jwt_token, controller_birkeland_payments.all_transactions);
 
 
 
