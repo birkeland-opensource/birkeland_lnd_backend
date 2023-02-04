540234e06b8e47a846bfe4b0882577cb73013082
rubansundararaj <rubansundararaj@gmail.com>
2023-02-04 16:37:27 +0800

    secure lnd ops endpoints

--- birkeland_router/lnd_operation_router.js
+++ birkeland_router/lnd_operation_router.js
@@ -8,19 +8,12 @@ const { unauth_lnd_ops } = require("../birkeland_controller/unauthenticated_birk
 const {create_node_auth_password, get_node_auth_token, check_endpoint_is_authenticated } = require("../node_authentication/node_auth_controller");
 const { decode_node_auth_jwt_token } = require("../support_functions/utils");
 
-apirouter.post("/auth_lnd_ops", auth_lnd_ops);
-apirouter.post("/unauth_lnd_ops", unauth_lnd_ops);
+apirouter.post("/auth_lnd_ops",decode_node_auth_jwt_token, auth_lnd_ops);
+apirouter.post("/unauth_lnd_ops",decode_node_auth_jwt_token, unauth_lnd_ops);
 
 apirouter.post("/get_node_auth_token", get_node_auth_token);
 
-apirouter.post("/create_node_auth_password",create_node_auth_password);
-
-
-
 apirouter.post("/check_endpoint_is_authenticated",decode_node_auth_jwt_token, check_endpoint_is_authenticated);
-
-
-
-apirouter.get("/get_lnd_dashboard_info", combined_lnd_grpc_operation.get_dashboard_info_from_birkeland_lnd);
+apirouter.get("/get_lnd_dashboard_info",decode_node_auth_jwt_token, combined_lnd_grpc_operation.get_dashboard_info_from_birkeland_lnd);
 
 module.exports = apirouter;
--- node_authentication/node_auth_controller.js
+++ node_authentication/node_auth_controller.js
@@ -3,107 +3,110 @@ const bcrypt = require("bcrypt");
 const { create_node_auth_jwt_token } = require("../support_functions/utils");
 const { v4: uuidv4 } = require("uuid");
 
-exports.check_endpoint_is_authenticated = async(req, res) => {
-    try{
-        return res.status(200).send({ success: true, message:"authenticated" });
-    }
-    catch(err){
+exports.check_endpoint_is_authenticated = async (req, res) => {
+  try {
+    return res.status(200).send({ success: true, message: "authenticated" });
+  } catch (err) {}
+};
 
+const create_node_auth_password = async (req, res) => {
+  try {
+    console.log(req.body);
+    var email = req.body.email_id;
+    const userTaken = await validateEmail(email);
+    if (userTaken) {
+      return res.status(400).json({
+        message: "Email in use reset your password",
+        success: false,
+      });
     }
-}
+    const hashedPassword = await bcrypt.hash(req.body.password, 8);
+    var userObject = {
+      email: email,
+      password: hashedPassword,
+      user_id: uuidv4(),
+      private_key: uuidv4(),
+      is_password_set: true,
+      date_created: new Date(),
+      date_updated: new Date(),
+    };
+    var result = await node_user_schema_item_model.create(userObject);
+    let payload = {
+      email: result["email"],
+      user_id: result["user_id"],
+    };
+    var auth_token = await create_node_auth_jwt_token(
+      payload,
+      result["private_key"]
+    );
+    console.log(result);
+    return res.status(201).send({
+      message: { node_auth_key: auth_token },
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: err.message,
+      success: false,
+    });
+  }
+};
 
-exports.create_node_auth_password = async (req, res) =>{
-    try{
-        var email = req.body.email;
-        const userTaken = await validateEmail(email);
-        if(userTaken) {
-            return res.status(400).json({
-                message: "Email in use reset your password",
-                success: false,
-            });
-        }
-        const hashedPassword = await bcrypt.hash(req.body.password, 8);
-        var userObject = {
-            email : email,
-            password :hashedPassword,
-            user_id : uuidv4(),
-            private_key : uuidv4(),
-            is_password_set  :true,
-            date_created : new Date(),
-            date_updated : new Date()
-        }
-        var result = await node_user_schema_item_model.create(userObject);
-        let payload = {
-            email : result["email"],
-            user_id : result["user_id"]
-        }
-        var auth_token = await create_node_auth_jwt_token(payload, result["private_key"])
-        console.log(result);
-        return res.status(201).send({
-            message: {"node_auth_key" : auth_token},
-            success: true,
-        });
-    }
-    catch (err) {
-        console.log(err);
-        return res.status(500).send({
-            message: err.message,
-            success: false,
-        });
-    }
-    }
-
-exports.get_node_auth_token = async (req, res) =>{
-
-    try{
-        var email = req.body.email;
-        var password = req.body.password;
-        const user = await node_user_schema_item_model.findOne({ email });
-        if(!user) {
-            return res.status(404).json({
-                message: "Failed login attempt",
-                email: "Incorrect email",
-                success: false,
-            })
-        }
-
+exports.get_node_auth_token = async (req, res) => {
+  try {
+    var email = req.body.email_id;
+    var password = req.body.password;
+    const current_users = await node_user_schema_item_model.find({});
+    console.log(current_users.length);
+    if (current_users.length <= 1 && email === current_users[0]["email"]) {
+      var user = await node_user_schema_item_model.findOne({ email });
+      if (user) {
         let isMatch = await bcrypt.compare(password, user.password);
-        if(isMatch) {
-            
-            let payload = {
-                email : user["email"],
-                user_id : user["user_id"]
-            }
+        if (isMatch) {
+          let payload = {
+            email: user["email"],
+            user_id: user["user_id"],
+          };
 
-            var token = await create_node_auth_jwt_token(payload,user["private_key"]);
+          var token = await create_node_auth_jwt_token(
+            payload,
+            user["private_key"]
+          );
 
-            return res.status(200).json({
-                message: {"node_auth_key" : token},
-                success: true
-            });
-        }
-        else {
-            return res.status(403).json({
-                message: "Failed login attempt",
-                success: false
-            })
-        }
-    }
-    catch(err){
-        return res.status(500).send({
-            message: err.message,
+          return res.status(200).json({
+            message: { node_auth_key: token },
+            success: true,
+          });
+        } else {
+          return res.status(403).json({
+            message: "Failed login attempt",
             success: false,
-        });
-
+          });
+        }
+      } else {
+        await create_node_auth_password(req, res);
+      }
+    } else {
+      return res.status(400).json({
+        message: "Wrong credentials",
+        success: false,
+      });
     }
-}
-
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      message: err.message,
+      success: false,
+    });
+  }
+};
 
 const validateEmail = async (email) => {
-    let user = await node_user_schema_item_model.findOne({ email });
-    if(user) {
-        return true;
-    } else {
-        return false;
-    }
+  let user = await node_user_schema_item_model.findOne({ email });
+  if (user) {
+    return true;
+  } else {
+    return false;
+  }
 };
