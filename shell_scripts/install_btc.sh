2c92766f22f15a94cceae854781e173b20d04d0d
rubansundararaj <rubansundararaj@gmail.com>
2023-02-07 15:01:38 +0800

    add pwd

--- shell_scripts/install_btc.sh
+++ shell_scripts/install_btc.sh
@@ -12,7 +12,7 @@ apt-get install libminiupnpc-dev -y
 apt-get install libzmq3-dev -y
 apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools -y
 apt-get install libqrencode-dev -y
-cd ~/code/bitcoin && ./contrib/install_db4.sh
+cd ~/code/bitcoin && ./contrib/install_db4.sh pwd
 cd ~/code/bitcoin && ./autogen.sh
 export BDB_PREFIX='/home/${USER}/code/bitcoin/db4'
 cd ~/code/bitcoin && ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
