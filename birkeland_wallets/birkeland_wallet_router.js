c2259b9c5b2f2ea149d55ddbb7ddb1c8443112cb
rubansundararaj <rubansundararaj@gmail.com>
2023-02-10 15:01:18 +0800

    get wallet info with read key

--- birkeland_wallets/birkeland_wallet_router.js
+++ birkeland_wallets/birkeland_wallet_router.js
@@ -10,7 +10,7 @@ apirouter.post("/wallet", decode_node_auth_jwt_token,birkeland_wallet_controller
 
 apirouter.get("/wallets_by_user_id",decode_node_auth_jwt_token, birkeland_wallet_controller.get_all_wallets_by_user_id);
 
-apirouter.get("/wallet",decode_node_auth_jwt_token, birkeland_wallet_controller.get_one_wallet);
+apirouter.get("/wallet",auth_birkeland_wallet_access, birkeland_wallet_controller.get_one_wallet);
 
 apirouter.get("/all_transactions",decode_node_auth_jwt_token, controller_birkeland_payments.all_transactions);
 
