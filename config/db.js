6e6dcbe620489af1d785049aca6d283249dca801
rubansundararaj <rubansundararaj@gmail.com>
2023-03-18 19:15:10 +0800

    talk to loal host

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 
 
 var mongoose = require('mongoose');
-const mongoatlasUrl = "mongodb+srv://elonmusk:ruban@cluster0.guvnoyy.mongodb.net/?retryWrites=true&w=majority";
+const mongoatlasUrl = "mongodb://localhost:27017/";
 mongoose.connect(mongoatlasUrl, {
 useNewUrlParser: true,
 useUnifiedTopology: true,
