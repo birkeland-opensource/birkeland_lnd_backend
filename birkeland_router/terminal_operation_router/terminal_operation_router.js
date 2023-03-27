46b6235040345cc93198ec6ffd30c69ca0038719
rubansundararaj <rubansundararaj@gmail.com>
2023-03-27 11:36:36 +0800

    secure the apis

--- birkeland_router/terminal_operation_router/terminal_operation_router.js
+++ birkeland_router/terminal_operation_router/terminal_operation_router.js
@@ -6,18 +6,12 @@ const apirouter = express.Router();
 const terminal_controller = require("../../birkeland_controller/terminal_operation/terminal_controller");
 const { decode_node_auth_jwt_token } = require("../../support_functions/utils");
 
-apirouter.post("/can_execute_in_terminal", terminal_controller.can_execute_in_terminal);
+apirouter.post("/update_birkeland_server",decode_node_auth_jwt_token,  terminal_controller.update_birkeland_server);
 
-apirouter.post("/check_node_monitor",  terminal_controller.check_hw_monitor_running);
-apirouter.post("/check_lnd",  terminal_controller.check_lnd_running);
-apirouter.post("/install_btc",  terminal_controller.install_btc);
-apirouter.post("/update_birkeland_server",  terminal_controller.update_birkeland_server);
-apirouter.post("/install_lnd",  terminal_controller.install_lnd);
-apirouter.post("/install_node_monitoring",  terminal_controller.install_node_monitoring);
 apirouter.post("/update_lnd_config",decode_node_auth_jwt_token, terminal_controller.update_lnd_config);
 apirouter.post("/restore_lnd_config",decode_node_auth_jwt_token, terminal_controller.restore_lnd_config);
 
-apirouter.post("/manage_a_process",  terminal_controller.manage_a_process);
+apirouter.post("/manage_a_process", decode_node_auth_jwt_token, terminal_controller.manage_a_process);
 apirouter.post("/setup_encrypted_config_for_grpc", decode_node_auth_jwt_token, terminal_controller.execute_lnd_comm_config_command )
 
 module.exports = apirouter;
