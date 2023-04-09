59128af750a4e50b3adc3a0f6a2e7da00d0fb91e
rubansundararaj <rubansundararaj@gmail.com>
2023-04-09 12:29:03 -0400

    add log

--- birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
+++ birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
@@ -4,7 +4,7 @@ const birkeland_lnd_custom_macaroon = require('birkeland_lnd_custom_macaroon')
 exports.get_dashboard_info_from_birkeland_lnd = async(req,res) =>{
 
   try{
-    
+    console.log({operation : "get_chain_balance",macaroon :req.headers["macaroon"]})
     let chain_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_chain_balance",macaroon :req.headers["macaroon"]});
       let channel_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_channel_balance",macaroon :req.headers["macaroon"]});
       let wallet_info = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_wallet_info",macaroon :req.headers["macaroon"]});
