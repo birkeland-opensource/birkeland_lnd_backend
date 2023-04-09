2a5f159d27f7247136f9cb9594bd97a628f00b7b
rubansundararaj <rubansundararaj@gmail.com>
2023-04-09 12:28:17 -0400

    get macaroon from header

--- birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
+++ birkeland_controller/lnd_grpc_operation/combined_lnd_grpc_operation.js
@@ -4,11 +4,11 @@ const birkeland_lnd_custom_macaroon = require('birkeland_lnd_custom_macaroon')
 exports.get_dashboard_info_from_birkeland_lnd = async(req,res) =>{
 
   try{
-
-    let chain_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_chain_balance",macaroon :req.body.macaroon});
-      let channel_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_channel_balance",macaroon :req.body.macaroon});
-      let wallet_info = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_wallet_info",macaroon :req.body.macaroon});
-      let wallet_version = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_wallet_version",macaroon :req.body.macaroon});
+    
+    let chain_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_chain_balance",macaroon :req.headers["macaroon"]});
+      let channel_balance = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_channel_balance",macaroon :req.headers["macaroon"]});
+      let wallet_info = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_wallet_info",macaroon :req.headers["macaroon"]});
+      let wallet_version = await birkeland_lnd_custom_macaroon.PerformAuthenticatedOperation({operation : "get_wallet_version",macaroon :req.headers["macaroon"]});
       let dash_board_info_object = {
         "chain_balance" :chain_balance,
         "channel_balance" :channel_balance,
