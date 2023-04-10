15ba9b756cd84061deb3a7318c587de64523367e
rubansundararaj <rubansundararaj@gmail.com>
2023-04-10 09:36:27 -0400

    subscription for htlc analysis

---
+++ htlc_analysis/subscribed_events.js
@@ -0,0 +1,44 @@
+const lnService = require('lightning');
+const fs = require("fs");
+
+const get_authenticated_lnd = () => {
+    const tls_cert = fs.readFileSync("/etc/birkeland/tlscert", {
+      encoding: "utf8",
+      flag: "r",
+    });
+
+    const macroon = fs.readFileSync("/etc/birkeland/btc_admin_macroon", {
+      encoding: "utf8",
+      flag: "r",
+    });
+
+    const { lnd } = authenticatedLndGrpc({
+      cert: tls_cert,
+      macaroon: macroon,
+      socket: "127.0.0.1:10009",
+    });
+
+    return lnd;
+  };
+
+
+const subscribeToForwards = async () => {
+
+ console.log("subscribeToForwards")   
+ let lnd = get_authenticated_lnd();
+  const forwardEventEmitter = lnService.subscribeToForwards({lnd});
+
+  forwardEventEmitter.on('forward', forward => {
+    console.log('Forward event:', forward);
+  });
+
+  forwardEventEmitter.on('error', err => {
+    console.error('Error:', err);
+  });
+
+  forwardEventEmitter.on('end', () => {
+    console.log('End of subscription');
+  });
+};
+
+subscribeToForwards();
